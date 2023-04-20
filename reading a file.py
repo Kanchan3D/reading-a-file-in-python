63c1260297e333bfbf5b5b839ee35d8c93d94799
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-04-20 19:02:55 +0530

    Add files via upload

---
+++ reading a file.py
@@ -0,0 +1,6 @@
+#opeaning file 'txt1.txt' in read mode
+f=open('txt1','r')
+#reading file with read() function
+print(f.read())
+#reading file with readline() function
+print(f.readline())#it will print noting as the file pointer is already is in the end
